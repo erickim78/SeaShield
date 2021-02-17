5799c561a2715b3edcea7369de54d28471dfa532
Andrew Berry <andrewberry@ucsb.edu>
2021-02-17 03:12:05 -0800

    AB - Changed UX and buttons to be what we need specificically, added a few colors to colors as well, also linked buttons to the corresponding java file to be filled with commands to send to the JetsonNano

--- app/src/main/java/com/example/seashield/ui/home/HomeFragment.java
+++ app/src/main/java/com/example/seashield/ui/home/HomeFragment.java
@@ -28,8 +28,7 @@ public class HomeFragment extends Fragment {
 
     public View onCreateView(@NonNull LayoutInflater inflater,
                              ViewGroup container, Bundle savedInstanceState) {
-        homeViewModel =
-                new ViewModelProvider(this).get(HomeViewModel.class);
+        homeViewModel = new ViewModelProvider(this).get(HomeViewModel.class);
         View root = inflater.inflate(R.layout.fragment_home, container, false);
 
         /*
@@ -43,22 +42,24 @@ public class HomeFragment extends Fragment {
 
          */
 
-        TextView hlog = (TextView) root.findViewById( R.id.homeLog );
+        TextView hlog = (TextView) root.findViewById( R.id.timeRunning );
         hlog.setMovementMethod( new ScrollingMovementMethod() );
 
 
-        Button btn1 = (Button) root.findViewById( R.id.startbutton );
-        Button btn2 = (Button) root.findViewById( R.id.stopbutton );
-        Button btn3 = (Button) root.findViewById( R.id.spraybutton );
-        Button btn4 = (Button) root.findViewById( R.id.resetbutton );
+        Button btn1 = (Button) root.findViewById( R.id.armButton );
+        Button btn2 = (Button) root.findViewById( R.id.disarmButton );
+        Button btn3 = (Button) root.findViewById( R.id.takeoffButton );
+        Button btn4 = (Button) root.findViewById( R.id.landButton );
+        Button btn5 = (Button) root.findViewById( R.id.hoverButton );
+        Button btn6 = (Button) root.findViewById( R.id.sprayButton );
 
 
         btn1.setOnClickListener( new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Log.i(APPNAME, "Start Button Pressed");
+                Log.i(APPNAME, "Arm Button Pressed");
                 //Communicate with Drone
-                hlog.append("\nTIME XX:XX- Drone Starting...");
+                hlog.append("\nTIME XX:XX- Drone Armed...");
             }
 
         });
@@ -66,14 +67,44 @@ public class HomeFragment extends Fragment {
         btn2.setOnClickListener( new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Log.i(APPNAME, "End Button Pressed");
+                Log.i(APPNAME, "Disarm Button Pressed");
                 //Communicate with Drone
-                hlog.append("\nTIME XX:XX- Drone Ending...");
+                hlog.append("\nTIME XX:XX- Drone Disarmed...");
             }
 
         });
 
         btn3.setOnClickListener( new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Log.i(APPNAME, "Takeoff Button Pressed");
+                //Communicate with Drone
+                hlog.append("\nTIME XX:XX- Drone Taking off...");
+            }
+
+        });
+
+        btn4.setOnClickListener( new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Log.i(APPNAME, "Land Button Pressed");
+                //Communicate with Drone
+                hlog.append("\nTIME XX:XX- Drone Landing...");
+            }
+
+        });
+
+        btn5.setOnClickListener( new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Log.i(APPNAME, "Hover Button Pressed");
+                //Communicate with Drone
+                hlog.append("\nTIME XX:XX- Drone Hovering...");
+            }
+
+        });
+
+        btn6.setOnClickListener( new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 Log.i(APPNAME, "Spray Button Pressed");
@@ -83,15 +114,6 @@ public class HomeFragment extends Fragment {
 
         });
 
-        btn4.setOnClickListener( new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                Log.i(APPNAME, "Reset Button Pressed");
-                //Communicate with Drone
-                hlog.append("\nTIME XX:XX- Drone Resetting...");
-            }
-
-        });
         return root;
     }
 }
